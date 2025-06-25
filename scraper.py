f146c42d3b5bebc2f01538871809b790e9ca4761
Yash Modi <modiyash2007@gmail.com>
2025-06-25 08:56:45 -0400

    Getting rid of URL user-agent error

--- scraper.py
+++ scraper.py
@@ -3,12 +3,11 @@ import sys
 
 url = "https://pixelford.com/blog/"
 try:
-    response = requests.get(url)
-
+    response = requests.get(url, headers = {'user-agent' : 'Hello world'})
+    print(response.content)
 except requests.exceptions.ConnectionError as e:
     print("Connection error. Please check your internet connection,", e)
     sys.exit()
 except requests.exceptions.Timeout as e:
-    print("Connection timed out. Please check your internet connection," e)
+    print("Connection timed out. Please check your internet connection,", e)
     sys.exit()
-print(response.content)
